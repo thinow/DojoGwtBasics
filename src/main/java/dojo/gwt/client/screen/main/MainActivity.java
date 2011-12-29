99fe674498520ebacde62c4e4e88c1f2697c5dbd
Thierry Nowak <thinow@gmail.com>
2011-12-29 13:11:51 +0100

    Utilisation du TimeService dans la MainActivity.

--- src/main/java/dojo/gwt/client/screen/main/MainActivity.java
+++ src/main/java/dojo/gwt/client/screen/main/MainActivity.java
@@ -5,12 +5,15 @@ import java.util.Date;
 import com.google.gwt.activity.shared.AbstractActivity;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.google.inject.Inject;
 
 import dojo.gwt.client.injection.ClientFactory;
 import dojo.gwt.client.place.MainPlace;
 import dojo.gwt.client.screen.main.MainView.Presenter;
+import dojo.gwt.shared.TimeServiceAsync;
 
 public class MainActivity extends AbstractActivity implements Presenter {
 
@@ -20,15 +23,16 @@ public class MainActivity extends AbstractActivity implements Presenter {
 	@Inject
 	private MainView view;
 
+	@Inject
+	private TimeServiceAsync timeService;
+
 	private MainPlace place;
 
 	public void start(AcceptsOneWidget parent, EventBus eventBus) {
 		view.setPresenter(this);
-		view.setWord(place.getToken());
+		view.addWord(place.getToken());
 
 		parent.setWidget(view);
-
-		GWT.log(getClass().getName() + " has started successfully !");
 	}
 
 	public void setPlace(MainPlace place) {
@@ -36,10 +40,26 @@ public class MainActivity extends AbstractActivity implements Presenter {
 	}
 
 	@Override
-	public void onButtonClick() {
+	public void onGoToButtonClick() {
 		String now = String.valueOf(new Date().getTime());
 
 		MainPlace nextPlace = new MainPlace(now);
 		clientFactory.getPlaceController().goTo(nextPlace);
+	}
+
+	@Override
+	public void onTimeButtonClick() {
+		timeService.getTime(new AsyncCallback<Date>() {
+			@Override
+			public void onSuccess(Date result) {
+				Window.alert(String.valueOf(result));
+			}
+
+			@Override
+			public void onFailure(Throwable caught) {
+				Window.alert("Erreur lors de la récupération de l'heure");
+				GWT.log("erreur !", caught);
+			}
+		});
 	}
 }
--- src/main/java/dojo/gwt/client/screen/main/MainView.java
+++ src/main/java/dojo/gwt/client/screen/main/MainView.java
@@ -9,26 +9,45 @@ import com.google.gwt.user.client.ui.PushButton;
 public class MainView extends FlowPanel {
 
 	public interface Presenter {
-		void onButtonClick();
+		/** Déclenchement d'un clic sur le bouton de parcours des pages. */
+		void onGoToButtonClick();
+
+		/** Déclenchement d'un clic sur le bouton de récupération de l'heure. */
+		void onTimeButtonClick();
 	}
 
 	private Presenter presenter;
 
 	public MainView() {
+		addTimeButton();
+		addGoToButton();
+
+		addWord("MainActivity !");
+	}
+
+	private void addTimeButton() {
+		PushButton button = new PushButton("What time is it please ?");
+		button.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				presenter.onTimeButtonClick();
+			}
+		});
+		add(button);
+	}
+
+	private void addGoToButton() {
 		PushButton button = new PushButton("Go to place...");
 		button.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
-				presenter.onButtonClick();
+				presenter.onGoToButtonClick();
 			}
 		});
 		add(button);
-
-		Label label = new Label("MainActivity !");
-		add(label);
 	}
 
-	public void setWord(String word) {
+	public void addWord(String word) {
 		add(new Label(word));
 	}
 
