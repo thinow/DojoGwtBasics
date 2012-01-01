c42dbe16d139722dfe796fdd8db0619f8b18af13
Thierry Nowak <thinow@gmail.com>
2012-01-01 17:52:53 +0100

    Propage les événements lors de la recherche des films dans la TopActivity.

--- src/main/java/dojo/gwt/client/screen/top/TopActivity.java
+++ src/main/java/dojo/gwt/client/screen/top/TopActivity.java
@@ -9,6 +9,8 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.google.inject.Inject;
 
+import dojo.gwt.client.event.async.finished.AsyncFinishedEvent;
+import dojo.gwt.client.event.async.started.AsyncStartedEvent;
 import dojo.gwt.client.place.TopPlace;
 import dojo.gwt.client.screen.top.TopView.Presenter;
 import dojo.gwt.shared.rpc.object.MovieReference;
@@ -24,8 +26,12 @@ public class TopActivity extends AbstractActivity implements Presenter {
 	@Inject
 	private MovieServiceAsync movieService;
 
+	private EventBus eventBus;
+
 	@Override
 	public void start(AcceptsOneWidget panel, EventBus eventBus) {
+		this.eventBus = eventBus;
+
 		panel.setWidget(view);
 		view.setPresenter(this);
 
@@ -34,17 +40,21 @@ public class TopActivity extends AbstractActivity implements Presenter {
 	}
 
 	private void startDataRetreiving() {
+		eventBus.fireEvent(new AsyncStartedEvent());
+
 		Integer count = place.getCount();
 		movieService.getTopMovies(count, new AsyncCallback<MovieReference[]>() {
 
 			@Override
 			public void onSuccess(MovieReference[] result) {
 				displayMovies(result);
+				eventBus.fireEvent(new AsyncFinishedEvent());
 			}
 
 			@Override
 			public void onFailure(Throwable caught) {
 				Window.alert("Erreur lors de la récupération des films");
+				eventBus.fireEvent(new AsyncFinishedEvent());
 			}
 		});
 	}
