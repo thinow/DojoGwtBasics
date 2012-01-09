47d7f6132a0067391f3f9583892ba8eb727607cf
Thierry Nowak <thinow@gmail.com>
2012-01-09 13:23:54 +0100

    Uniformise le style sur les titres des pages et les boutons.

--- src/main/java/dojo/gwt/client/screen/menu/MenuViewImpl.java
+++ src/main/java/dojo/gwt/client/screen/menu/MenuViewImpl.java
@@ -2,12 +2,24 @@ package dojo.gwt.client.screen.menu;
 
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.PushButton;
 
 public class MenuViewImpl extends FlowPanel implements MenuView {
 
 	@SuppressWarnings("unused")
 	private Presenter presenter;
+
+	public MenuViewImpl() {
+		addPageTitle();
+	}
+
+	private void addPageTitle() {
+		Label title = new Label("Menu");
+		title.addStyleName("pageTitle");
+
+		this.add(title);
+	}
 
 	@Override
 	public void addButton(String buttonText, ClickHandler handler) {
