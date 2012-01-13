2c6036ab255d7432f4acac1e740769e60c428f66
Thierry Nowak <thinow@gmail.com>
2012-01-13 14:06:44 +0100

    Mapping View/ViewImpl depuis le module GWT.

--- src/main/java/dojo/gwt/client/injection/InjectorBinder.java
+++ src/main/java/dojo/gwt/client/injection/InjectorBinder.java
@@ -3,21 +3,11 @@ package dojo.gwt.client.injection;
 import com.google.gwt.inject.client.AbstractGinModule;
 import com.google.inject.Singleton;
 
-import dojo.gwt.client.screen.add.AddView;
-import dojo.gwt.client.screen.add.AddViewImpl;
-import dojo.gwt.client.screen.menu.MenuView;
-import dojo.gwt.client.screen.menu.MenuViewImpl;
-import dojo.gwt.client.screen.top.TopView;
-import dojo.gwt.client.screen.top.TopViewImpl;
-
 public class InjectorBinder extends AbstractGinModule {
 
 	@Override
 	protected void configure() {
 		bind(ClientFactory.class).in(Singleton.class);
-		bind(MenuView.class).to(MenuViewImpl.class);
-		bind(TopView.class).to(TopViewImpl.class);
-		bind(AddView.class).to(AddViewImpl.class);
 	}
 
 }
